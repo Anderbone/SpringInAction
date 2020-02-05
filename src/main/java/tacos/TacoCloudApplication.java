fd930d89755800353e990fbcdc13d1a844b18a7d
Anderbone <yanchiyu@outlook.com>
2020-02-05 11:13:54 +0000

    changed the mode of database

--- src/main/java/tacos/TacoCloudApplication.java
+++ src/main/java/tacos/TacoCloudApplication.java
@@ -1,7 +1,11 @@
 package tacos;
 
+import org.h2.tools.Server;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+
+import java.sql.SQLException;
 
 @SpringBootApplication
 //@Configuration
@@ -11,6 +15,11 @@ public class TacoCloudApplication{
 
     public static void main(String[] args) {
         SpringApplication.run(TacoCloudApplication.class, args);
+    }
+
+    @Bean(initMethod = "start", destroyMethod = "stop")
+    public Server h2Server() throws SQLException {
+        return Server.createTcpServer("-tcp", "-tcpAllowOthers", "-tcpPort", "9092");
     }
 
 }
